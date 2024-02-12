865243d0b8eb635cf9d114deb7b5de76beab91a3
Gregory Nelson Esguerra <gregory_keyboard@outlook.com>
2024-02-12 22:08:35 +0800

    Add project files.

---
+++ AutoDefaultStructs/Instructor.cs
@@ -0,0 +1,14 @@
+﻿namespace AutoDefaultStructs
+{
+    public struct Instructor
+    {
+        public int FacultyID { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public Instructor(string name)
+        {
+            Name = name;
+        }
+    }
+}
---
+++ AutoDefaultStructs/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace AutoDefaultStructs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Instructor instructor = new Instructor("Greg Esguerra");
+
+            //by default, age will be zero
+            Console.WriteLine($"Instructor's name is {instructor.Name}, Age is: {instructor.Age}");
+
+            Console.ReadLine();
+        }
+    }
+}
